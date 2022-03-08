8efe66c1934f283fedf7d5138b362808fba142ea
ranhquama <101180364+ranhquama@users.noreply.github.com>
2022-03-08 18:27:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ const contentCard = 'Chúc Cô giáo tuyệt vời của tụi Em ngày 8/3 th�
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hot-girl.png';
+const giftImage = 'hot-girl-png.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
